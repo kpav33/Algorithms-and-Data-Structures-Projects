0d9115cab1f1326fedaad0cd60fdbe7079df8154
Klemen <klemenpavlovic@gmail.com>
2021-01-22 14:46:02 +0100

    Palindrome checker project solution

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Algorithms-and-Data-Structures-Projects
 freeCodeCamp JavaScript Algorithms and Data Structures Projects
+
+Palindrome checker
+Assignement: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/palindrome-checker
+Official solution: https://forum.freecodecamp.org/t/freecodecamp-challenge-guide-palindrome-checker/16004
---
+++ palindrome-checker.js
@@ -0,0 +1,24 @@
+function palindrome(str) {
+    //Remove all non-alphanumeric characters and turn the resulting array into a lower cased string
+    let regex = /[A-Za-z0-9]/gi;
+    let arr = str.match(regex).join("").toLowerCase();
+    //Create two arrays, one original one reversed
+    let reversedArr = arr.split("").reverse();
+    let originalStringArr = arr.split("");
+    //console.log(arr);
+    //Check for palindromes
+    let count = 0;
+    for (let i = 0; i < originalStringArr.length; i++) {
+      if (reversedArr[i] === originalStringArr[i]) {
+        count++;
+        //console.log(count);
+      }
+    }
+    if (originalStringArr.length === count) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+palindrome("2A3*3a2");
